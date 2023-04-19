46c812f60e6428df598665a5bab2c577025a5fca
eduardo pitz <pitz.eduardo@gmail.com>
2023-04-19 07:46:37 -0300

    Update working_with_columns.py

--- working_with_columns.py
+++ working_with_columns.py
@@ -1,36 +1,26 @@
 # WORKING WIHT COLUMNS
 
 blocked_tax_id_runs = (
-  spark.table("br__contract.jumanji__runs")
-       .where(F.col("run__tax_id") == "yy")
-       .where(F.col("run__affinity_type") == "xx")
-       .withColumn("without_conclusion", F.when(F.col("run__finished_at").isNull(), 0).otherwise(1))
-       .withColumnRenamed("run__affinity_id", "affinity_id")
-       .drop(F.col("run__finished_at"))
-       .select("run__flow").summary() 
-       .display(20))
+    spark.table("br__contract.jumanji__runs")
+    .where(F.col("run__tax_id") == "yy")
+    .where(F.col("run__affinity_type") == "xx")
+    .withColumn("without_conclusion", F.when(F.col("run__finished_at").isNull(), 0).otherwise(1))
+    .withColumnRenamed("run__affinity_id", "affinity_id")
+    .drop(F.col("run__finished_at"))
+    .select("run__flow").summary()
+    .display(20))
 
-# RENAMING COLUMNS
-# Do you want to rename a column?
-# You can do that using .alias() or .withColumnRenamed("run__affinity_id", "affinity_id"). I personally prefer using .withColumnRenamed("run__affinity_id", "affinity_id")
+# Reads the data from the table using the spark.table() function.
+# Filters the data to include only those rows where the value in the run__tax_id column is equal to "yy" and 
+# the value in the run__affinity_type column is equal to "xx".
+# Adds a new column to the DataFrame named "without_conclusion" using the withColumn() function. 
+# This column is computed using the when() and otherwise() functions from the pyspark.sql.functions module. 
+# It sets the value to 0 if the value in the run__finished_at column is null and 1 otherwise.
+# Renames the column run__affinity_id to affinity_id using the withColumnRenamed() function.
+# Drops the run__finished_at column using the drop() function.
+# Selects the run__flow column from the DataFrame and computes summary statistics using the summary() function.
+# Displays the first 20 rows of the resulting summary DataFrame using the display() function.
 
-# REMOVING COLUMNS
-# Do you want to remove a column?
-# You can use .drop() to remove columns instead of selecting all columns that you want with the .select()
-
-# DESCRIBE (count, mean, stddev, min, max)
-# .describe() will perform some calculations and delivery these metrics for you: count, mean, stddev, min, max
-
-# SUMMARY 
-# Almost the same as .describe() but with more flexibility and some percentiles.
-# Also, you can define with metrics you want to see: .select("run__flow").summary("min", "10%", "90%") 
-
-# Seems that .describe() and .summary() can present different results, depending on PySpark version. Nice, isn't?
-
-
-
-
-
-
-
-
+# Overall, this query is used to filter, transform, and summarize data from a PySpark DataFrame in 
+# order to obtain a summary of blocked tax ID runs that have not been concluded. The summary includes 
+# various statistical measures such as count, mean, and standard deviation of the run__flow column.
